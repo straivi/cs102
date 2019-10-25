f9b86eafaaf23e54646d4e251f7c188b497b1fe6
matvey <bme15122000@gmail.com>
2019-10-25 16:27:36 +0300

    реализованна функция group

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -28,7 +28,7 @@ def group(values: List[str], n: int) -> List[List[str]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    pass
+    return [values[symbolPoz: symbolPoz + n] for symbolPoz in range(0, len(values), n)]
 
 
 def get_row(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
