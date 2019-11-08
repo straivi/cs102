569c83adb807c5d65116a3f97ca37bd155e23cff
matvey <bme15122000@gmail.com>
2019-11-08 15:49:17 +0300

    реализованы все методы файла live_proto

--- homework03/life_proto.py
+++ homework03/life_proto.py
@@ -59,7 +59,7 @@ class GameOfLife:
             # Выполнение одного шага игры (обновление состояния ячеек)
             # PUT YOUR CODE HERE
 
-            draw_grid()
+            self.draw_grid()
             pygame.display.flip()
             clock.tick(self.speed)
         pygame.quit()
@@ -84,7 +84,7 @@ class GameOfLife:
         """
         for row in range(0, self.height/self.cell_size):
             for coll in range(0, self.width/self.cell_size):
-                Grid[row][col] = random.getrandbits(1)
+                Grid[row][coll] = random.getrandbits(1)
         return Grid
 
     def draw_grid(self) -> None:
@@ -94,7 +94,7 @@ class GameOfLife:
         for row in range(0, self.height, self.cell_size):
             for coll in range(0, self.width, self.cell_size):
                 color = pygame.Color('white') if Grid[row/self.cell_size][coll/self.cell_size] == 0 else pygame.Color('green')
-                pygame.draw.rect(screen, color, (row, coll, self.cell_size, self.cell_size))
+                pygame.draw.rect(self.screen, color, (row, coll, self.cell_size, self.cell_size))
         pass
 
     def get_neighbours(self, cell: Cell) -> Cells:
@@ -115,7 +115,22 @@ class GameOfLife:
         out : Cells
             Список соседних клеток.
         """
-        pass
+        cells = []
+        for row in range(cell[0]-1, cell[0]+1):
+            for coll in range(cell[1]-1, cell[1]+1):
+                if row < 0 or row == cell[0]: 
+                    continue
+                if coll < 0 or coll == cell[1]:
+                    continue
+                cells.append(Grid[row][coll])
+        return cells
+
+    def get_count_live_neighbours(self, cells: Cells) -> int:
+        liveNeighb = 0
+        for neighbour in range(len(cells)):
+            if cells[neighbour] == 1: 
+                liveNeighb += 1
+        return liveNeighb
 
     def get_next_generation(self) -> Grid:
         """
@@ -126,4 +141,15 @@ class GameOfLife:
         out : Grid
             Новое поколение клеток.
         """
+        GridCountOfNeighb = List[List[int]]
+        for row in range(len(Grid)):
+            for coll in range(len(Grid[0])):
+                cord = Tuple[row, coll]
+                GridCountOfNeighb[row][coll] = self.get_count_live_neighbours(self.get_neighbours(cord))
+        for row in range(len(Grid)):
+            for coll in range(len(Grid[0])):
+                if GridCountOfNeighb[row][coll] == 2 or GridCountOfNeighb[row][coll] == 3:
+                    Grid[row][coll] == 1
+                else: 
+                    Grid[row][coll] == 0
         pass
