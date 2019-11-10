123dd5e330eee9f5798caed617cbb7204ed35bd9
matvey <bme15122000@gmail.com>
2019-11-10 23:13:39 +0300

    consone game realisation

--- homework03/life-console.py
+++ homework03/life-console.py
@@ -11,13 +11,33 @@ class Console(UI):
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
+        screen.border('|', '|', '-', '-', '+', '+', '+', '+')
         pass
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
+        for row in range(self.life.rows):
+            for coll in range(self.life.cols):
+                simbol = '@' if self.life.curr_generation[row][coll] == 1 else ' '
+                try:
+                    screen.addstr(row + 1, coll + 1, simbol)
+                except curses.error:
+                    pass
         pass
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
+        curses.curs_set(0)
+        self.life.curr_generation = self.life.create_grid(True)
+        running = True
+        while running and self.life.is_changing and not self.life.is_max_generations_exceed:
+            screen.clear()
+            self.draw_borders(screen)
+            self.draw_grid(screen)
+            self.life.step()
+            screen.refresh()
         curses.endwin()
+
+if __name__ == '__main__':
+    gui = Console(GameOfLife((100, 100), True, 50))
+    gui.run()
