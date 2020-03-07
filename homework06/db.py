283616753dec51f77e2b6eb767fe92090695ee0e
matvey <bme15122000@gmail.com>
2020-03-07 11:36:43 +0300

    add hw6

---
+++ homework06/db.py
@@ -0,0 +1,37 @@
+from scraputils import get_news
+
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+engine = create_engine("sqlite:///news.db")
+session = sessionmaker(bind=engine)
+
+
+class News(Base):
+    __tablename__ = "news"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+    url = Column(String)
+    comments = Column(Integer)
+    points = Column(Integer)
+    label = Column(String)
+
+
+Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":
+    s = session()
+    news_list = get_news("https://news.ycombinator.com/", n_pages=35)
+    for post in news_list:
+        news = News(title=post['title'],
+                    author=post['author'],
+                    url=post['url'],
+                    comments=post['comments'],
+                    points=post['points'])
+        s.add(news)
+        s.commit()
